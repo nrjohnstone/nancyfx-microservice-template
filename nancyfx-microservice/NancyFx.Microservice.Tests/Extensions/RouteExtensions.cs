43e10d742bad64a7638d768bc082bc247c910ee6
nrjohnstone <locotewz@gmail.com>
2016-07-19 08:59:26 +0200

    add an example of unit testing a route without going through the http request supplied by the nancy fx test utilities

---
+++ nancyfx-microservice/NancyFx.Microservice.Tests/Extensions/RouteExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Nancy.Routing;
+
+namespace NancyFx.Microservice.Tests.Extensions
+{
+    public static class RouteExtensions
+    {
+        public static IEnumerable<Route> WithDescription(this IEnumerable<Route> instance, string method, string path)
+        {
+            var routes = instance.Where(
+                 x => x.Description.Method.Equals(method) &&
+                 x.Description.Path.Equals(path));
+            return routes;
+        }    
+    }
+}
--- nancyfx-microservice/NancyFx.Microservice.Tests/Modules/EchoModuleTests.cs
+++ nancyfx-microservice/NancyFx.Microservice.Tests/Modules/EchoModuleTests.cs
@@ -1,13 +1,22 @@
 ﻿using System.Linq;
 using System.Threading;
 using FluentAssertions;
+using Nancy;
+using Nancy.Routing;
+using Nancy.Routing.Trie;
 using NancyFx.Microservice.Modules;
+using NancyFx.Microservice.Tests.Extensions;
 using Xunit;
 
 namespace NancyFx.Microservice.Tests.Modules
 {
     public class EchoModuleTests
     {
+        private EchoModule CreateSut()
+        {
+            return new EchoModule();
+        }
+
         [Fact]
         public void Should_reply_with_parameter_as_string()
         {
@@ -22,19 +31,23 @@ namespace NancyFx.Microservice.Tests.Modules
         }
 
         [Fact]
-        public void Should()
+        public void Example_of_unit_testing_a_route_without_http_requests()
         {
             var sut = CreateSut();
-            
+            var route = sut.Routes
+                .WithDescription("GET", @"/echo/{Text}").First();
+            var expectedResponseText = "SomeTestText";
+            DynamicDictionary parameters = new DynamicDictionary()
+            {
+                { "Text", expectedResponseText }
+            };
 
             // act
-            var route = sut.Routes.Where(x => x.Description.Equals("GET")).FirstOrDefault();
-
+            string routeResponse = route.Invoke(parameters, new CancellationToken()).Result;
+                        
             // assert
+            routeResponse.As<string>().Should().Be(expectedResponseText);
         }
-        private EchoModule CreateSut()
-        {
-            return new EchoModule();
-        }
+
     }
 }
