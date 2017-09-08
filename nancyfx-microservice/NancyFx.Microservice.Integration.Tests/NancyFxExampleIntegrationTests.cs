aa03df9d75bb242e038505fcf6f64c90e8958376
nrjohnstone <locotewz@gmail.com>
2017-09-08 08:09:28 +0200

    add some more test examples for POST and query strings

--- nancyfx-microservice/NancyFx.Microservice.Integration.Tests/NancyFxExampleIntegrationTests.cs
+++ nancyfx-microservice/NancyFx.Microservice.Integration.Tests/NancyFxExampleIntegrationTests.cs
@@ -1,5 +1,9 @@
-﻿using Autofac;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Autofac;
+using CsQuery.ExtensionMethods;
 using FluentAssertions;
+using Nancy;
 using Nancy.Testing;
 using NancyFx.Microservice.Example;
 using NancyFx.Microservice.Infrastructure;
@@ -105,5 +109,48 @@ namespace NancyFx.Microservice.Integration.Tests
             // assert
             result.Body.AsString().Should().Be(expectedPort);
         }
+
+        [Fact]
+        public void Example_of_testing_a_post_route_that_takes_a_JSON_body()
+        {
+            var bootstrapper = new TestBootstrapper(_mocks);
+            var browser = new Browser(bootstrapper);
+            
+            // act            
+            var result = browser.Post("/orders/", (with) => {
+                with.HttpRequest();
+                with.JsonBody(new { Supplier = "Foo"});                
+            });
+
+            // assert
+            result.StatusCode.Should().Be(HttpStatusCode.Created);
+        }
+
+        [Fact]
+        public void Example_of_testing_get_route_that_takes_parameters()
+        {
+            var bootstrapper = new TestBootstrapper(_mocks);
+            var browser = new Browser(bootstrapper);
+            
+            // act
+            var result = browser.Get("/orders", with =>
+            {
+                with.HttpRequest();
+                with.Query("supplier","Foo");
+            });
+
+            // assert
+            result.StatusCode.Should().Be(HttpStatusCode.OK);
+            var orderResponses = result.Body.DeserializeJson<IEnumerable<OrderResponseDto>>().ToList();
+            orderResponses.Should().NotBeNull();
+            orderResponses.Count.Should().Be(1);
+            orderResponses.First().Supplier.Should().Be("Foo");
+        }
     }
+
+    internal class OrderResponseDto
+    {
+        public string Supplier { get; set; }
+    }
+    
 }
---
+++ nancyfx-microservice/NancyFx.Microservice/Modules/OrderExampleModule.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using Nancy;
+using Nancy.ModelBinding;
+
+namespace NancyFx.Microservice.Modules
+{
+    public class NewOrderDto
+    {
+        public string Supplier { get; set; }
+    }
+
+    public class OrderDto
+    {
+        public string Supplier { get; set; }
+    }
+
+    public class OrderExampleModule : NancyModule
+    {
+        public OrderExampleModule()
+        {
+            Post["/orders"] = _ => CreateNewOrder();
+            Get["/orders"] = _ => GetOrders();
+        }
+
+        private Response GetOrders()
+        {
+            string supplierFilter = this.Request.Query["supplier"];
+
+            List<OrderDto> orders = new List<OrderDto>();
+            orders.Add(new OrderDto() { Supplier = "Foo"});
+            orders.Add(new OrderDto() { Supplier = "Bar" });
+
+            return Response.AsJson(orders.Where(x => x.Supplier.Equals(supplierFilter)));
+        }
+
+        private object CreateNewOrder()
+        {
+            NewOrderDto newOrderDto = this.Bind();
+
+            if (newOrderDto == null)
+                throw new Exception();
+
+            if (String.IsNullOrEmpty(newOrderDto.Supplier))
+                throw new Exception();
+            
+            return HttpStatusCode.Created;
+        }
+    }
+}
